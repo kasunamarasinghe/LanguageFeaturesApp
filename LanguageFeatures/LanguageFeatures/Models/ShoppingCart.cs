299cd7fb7116b6a532b72500dbedc6a556be2e26
kasunamarasinghe <kasun@davton.com>
2015-09-21 17:57:22 +0530

    Applying Extension Methods to an Interface

--- LanguageFeatures/LanguageFeatures/Models/ShoppingCart.cs
+++ LanguageFeatures/LanguageFeatures/Models/ShoppingCart.cs
@@ -2,11 +2,26 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Collections;
 
 namespace LanguageFeatures.Models
 {
-    public class ShoppingCart
+    public class ShoppingCart:IEnumerable<Product> 
     {
             public List<Product> Products { get; set; }
+
+            public IEnumerator<Product> GetEnumerator()
+            {
+
+                return Products.GetEnumerator();
+
+            }
+
+            IEnumerator IEnumerable.GetEnumerator()
+            {
+
+                return GetEnumerator();
+
+            }
     }
 }
