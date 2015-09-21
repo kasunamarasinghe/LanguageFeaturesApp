ddb37f549c3b647f390d172430289a41b1541b91
kasunamarasinghe <kasun@davton.com>
2015-09-21 17:09:47 +0530

    Reverting from an Automatic to a Regular Property

--- LanguageFeatures/LanguageFeatures/Controllers/HomeController.cs
+++ LanguageFeatures/LanguageFeatures/Controllers/HomeController.cs
@@ -1,9 +1,9 @@
-﻿using System;
+﻿using LanguageFeatures.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-
 namespace LanguageFeatures.Controllers
 {
     public class HomeController : Controller
@@ -15,6 +15,26 @@ namespace LanguageFeatures.Controllers
         {
 
             return "Navigate to a URL to show an example";
+
+        }
+        public ViewResult AutoProperty()
+        {
+
+            // create a new Product object 
+
+            Product myProduct = new Product();
+
+            // set the property value 
+
+            myProduct.Name = "Food";
+
+            // get the property 
+
+            string productName = myProduct.Name;
+
+            // generate the view 
+
+            return View("Result",(object)String.Format("Product name: {0}", productName));
 
         }
 
